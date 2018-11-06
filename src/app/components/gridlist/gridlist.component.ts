c094a97ba2a2a27642efe015cebf993c0f27a057
SKHAIRI MOHAMED AIMANE <skhairimedaimane@gmail.com>
2018-11-06 23:39:33 +0000

    Use the Gridlist component for both home/dashboard pages

--- src/app/components/gridlist/gridlist.component.ts
+++ src/app/components/gridlist/gridlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-gridlist',
@@ -7,17 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridlistComponent implements OnInit {
 
+  @Input() page: any; 
+
   tiles: Tile[];
 
-  constructor() { }
+  constructor(private zone: NgZone) { }
 
   ngOnInit() {
-    this.tiles = [
-      {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-      {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-      {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-      {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-    ];
+    this.zone.run(() => {
+      if (this.page === 1) {
+        console.log('dash');
+        this.tiles = [
+          { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+          { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+          { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+          { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+        ];
+      } else if (this.page === 2) {
+        console.log('home');
+        this.tiles = [
+          { text: 'One', cols: 4, rows: 1, color: 'lightblue' },
+          { text: 'Two', cols: 4, rows: 1, color: 'lightgreen' },
+          { text: 'Three', cols: 4, rows: 1, color: 'lightpink' },
+          { text: 'Four', cols: 4, rows: 1, color: '#DDBDF1' },
+        ];
+      } else {
+        console.log('notfoundpage');
+      }
+    });
   }
 
 }
