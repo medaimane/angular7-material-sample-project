d5846fd07e35614fa9be5e4f13c92210b4beb337
SKHAIRI MOHAMED AIMANE <skhairimedaimane@gmail.com>
2018-11-06 22:27:33 +0000

    Setup route for home component containes a gridlist

--- src/app/gridlist/gridlist.component.ts
+++ src/app/gridlist/gridlist.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridlistComponent implements OnInit {
 
+  tiles: Tile[];
+
   constructor() { }
 
   ngOnInit() {
+    this.tiles = [
+      {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+      {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+      {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+      {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    ];
   }
 
+}
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
 }
